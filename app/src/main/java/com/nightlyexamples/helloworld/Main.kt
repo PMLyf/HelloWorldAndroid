62026d789385b10362d7be441ac6ae6e99481be9
PMLyf <david.irvinge@gmail.com>
2022-04-27 08:56:56 -0400

    Step 1 - Step 1- Main activity must extend AppCompatActivity()

---
+++ app/src/main/java/com/nightlyexamples/helloworld/Main.kt
@@ -0,0 +1,18 @@
+package com.nightlyexamples.helloworld
+/**
+ *
+ *
+ * This is the first in a series of example Android applications publicly available on github with a little flavor.*
+ *
+ * Purpose of this tutorial: Display a textView with "Hello World" Centered on an activity.
+ *
+ * The next example will be moving to use of a fragment instead of an activity.
+ *
+* **/
+
+
+/**
+ * Step 1- Main activity must extend AppCompatActivity()
+ * */
+class Main {
+}
